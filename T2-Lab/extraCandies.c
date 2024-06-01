640ad6d2e108a7f1d140798b714a6e189ed1c839
RelexSun <88622289+RelexSun@users.noreply.github.com>
2024-06-01 22:15:41 +0700

    extra candies exercise

--- T2-Lab/extraCandies.c
+++ T2-Lab/extraCandies.c
@@ -1,18 +1,45 @@
 #include<stdio.h>
+#include <stdbool.h> 
+
+const char* boolToString(bool value) { // convert bool to string
+    return value ? "true" : "false";
+}
 
 int main() {
-  int candies[10], extraCandies, numOfKid;
+  int extraCandies, numOfKid;
   printf("Input the numbers of kids: ");
   scanf("%d", &numOfKid);
+    printf("Input the numbers of extraCandies: ");
+  scanf("%d", &extraCandies);
+
+  int candies[numOfKid];
   for (int i = 0; i < numOfKid; i++) {
-    printf("Input numbers of candies for %dth kid: ");
+    printf("Input numbers of candies for %dth kid: ", i + 1);
     scanf("%d", &candies[i]);
   }
 
   int length = sizeof(candies)/sizeof(candies[0]);
+  printf("The length is %d\n", length);
+  bool boolArray[numOfKid];
+  
+  int temp = candies[0];
+  for (int i = 0; i < length; i++) {
+    if (temp < candies[i]) {
+      temp = candies[i];
+    }
+  }
+  printf("largest is %d\n", temp);
 
   for (int i = 0; i < length; i++) {
-    
+    if (candies[i] + extraCandies >= temp) {
+      boolArray[i] = true;
+    } else {
+      boolArray[i] = false;
+    }
+  }
+
+  for (int i = 0; i < length; i++) {
+    printf("Kid %d can have the greatest number of candies: %s\n", i + 1, boolToString(boolArray[i]));
   }
 
   return 0;
