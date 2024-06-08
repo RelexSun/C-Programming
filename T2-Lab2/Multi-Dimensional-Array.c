398438e511162f55c248f3a0f20955bc64c5c6ea
RelexSun <88622289+RelexSun@users.noreply.github.com>
2024-06-08 10:13:01 +0700

    ...

--- T2-Lab2/Multi-Dimensional-Array.c
+++ T2-Lab2/Multi-Dimensional-Array.c
@@ -22,14 +22,30 @@ void subArrays (int size, int array1[size], int array2[size], int result[size]) 
 
 void resultArray (int size, int result[size]) {
     for (int i = 0; i < size; i++) {
-    printf("%d ", result[i]);
+    if (i == size - 1) {
+      printf("%d", result[i]);
+    } else {
+      printf("%d, ", result[i]);
+    }
   }
 }
 
 int main() {
-  int size;
-  printf("Enter numbers of element: ");
-  scanf("%d", &size);
+  int sizeArray1, sizeArray2, size;
+
+  while(1) {
+    printf("Enter numbers of element array 1: ");
+    scanf("%d", &sizeArray1);
+    printf("Enter numbers of element array 2: ");
+    scanf("%d", &sizeArray2);
+
+    if (sizeArray2 == sizeArray1) {
+      size = sizeArray1;
+      break;
+    }
+  }
+
+
 
   int array1[size], array2[size], sumResult[size], subResult[size];
 
@@ -39,11 +55,13 @@ int main() {
   sumArrays(size, array1, array2, sumResult);
   subArrays(size, array1, array2, subResult);
 
-  printf("The result of sum is: ");
+  printf("The result of sum is: {");
   resultArray(size, sumResult);
+  printf("}");
 
-  printf("\nThe result of sub is: ");
+  printf("\nThe result of sub is: {");
   resultArray(size, subResult);
+  printf("}");
 
 
   return 0;
